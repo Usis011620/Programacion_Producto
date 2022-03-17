fb206f0af7098c81e9b817f5fa5e7a0de177fde9
juliojc83 <vserpas22@gmail.com>
2022-03-17 14:12:44 -0600

    PHP

--- primero-proyecto-a1/private/db/config.php
+++ primero-proyecto-a1/private/db/config.php
@@ -2,15 +2,15 @@
 class DB{
     private $conexion, $result;
 
-    public function DB{$server, $user, $pass}{
-        $this->conexion = new PDO{$server, $user, $pass, array}{PDO::ATTR_EMULATE_PREPARES => false,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION}} or die{'Error al conectar con la base de datos'};
+    public function DB($server, $user, $pass){
+        $this->conxion = new PDO($server, $user, $pass, array(PDO::ATTR_EMULATE_PREPARES => false, 
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)) or die('Error al conectar con la base de datos');
     }
-    public function consultas{$sql=''}{
+    public function consultas($sql=''){
         try{
             $parametros = func_get_args();//Obtiene los parametros de la consulta SQL
-            array_shift($parametros);//Elimina el primer parametro que es la consulta SLQ
-            $this->preparado = $this->conexion->prepare($sql);
+            array_shift($parametros);//Elimina el primer parametro que es la consulta SQL
+            $this->preparado = $this->conxion->prepare($sql);
             $this->result = $this->preparado->execute($parametros); //Ejecuta la consulta SQL
         }catch(PDOException $e){
             echo 'Error '.$e->getMessage();
@@ -22,8 +22,8 @@ class DB{
     public function obtener_respuesta(){
         return $this->result;//Devuelve true si es exitoso o false si no
     }
-    public function obtener_UltimoId(){
-        return $this->conexion->lastInsertId();//Devuelve el ultimo id insertado 
+    public function obtenerUltimoId(){
+        return $this->conxion->lastInsertId();//Devuelve el ultimo id insertado
     }
 }
 ?>
